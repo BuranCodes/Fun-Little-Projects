ded04f86928bb070bd63fabecadd53c13b1ac820
BuranCodes <95971666+BuranCodes@users.noreply.github.com>
2023-01-14 19:50:35 +0100

    Create string_censor.cpp

---
+++ c++/string_censor.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <regex>
+
+int main(){
+std::string str {"hello I am Bob, show me your wallet now!"};
+std::regex reg("(bob|wallet)", std::regex_constants::icase); // ignore upper or lowercase
+std::cout << "Old string: " << str << "\nNew string: ";
+str = std::regex_replace(str, reg, "****");
+std::cout << str;
+}
