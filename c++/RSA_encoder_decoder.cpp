7e50a9e8e09d592e12876f0fe6e69dfb939b1e21
RadonCoding <86915746+RadonCoding@users.noreply.github.com>
2023-02-18 22:21:56 +0200

    Improvements and bug fixing.
    The problem with the code was that it was using std::pow and std::fmod which caused it not to work. This can be fixed by making our own function for it. I've used ChatGPT to generate said function out of laziness and it might have some room for improvement.

--- c++/RSA_encoder_decoder.cpp
+++ c++/RSA_encoder_decoder.cpp
@@ -9,31 +9,50 @@
 #include <iostream>
 #include <cmath>
 
-void encryption(std::string testString, std::vector<uint64_t>& encryptedMessages, int ePublicKey, int nPublicKey){
-    for (char c : testString) {
-    encryptedMessages.push_back(std::fmod(std::pow(int(c), ePublicKey), nPublicKey));
+// CODE FROM CHATGPT
+int64_t modPow(int64_t base, int64_t exponent, const int64_t modulus) {
+    int64_t result = 1;
+
+    while (exponent > 0) {
+        if (exponent % 2 == 1) {
+            result = (result * base) % modulus;
+        }
+        base = (base * base) % modulus;
+        exponent = exponent / 2;
     }
+    return result;
 }
 
-std::string decryption(std::vector<uint64_t> testEncryptedMessage, int dPrivateKey, int nPublicKey) {
-    std::string decryptedMsg;
-    for (int i : testEncryptedMessage) {
-        decryptedMsg.push_back(char(std::fmod((std::pow(i, dPrivateKey)), nPublicKey))); // number too big!
+std::vector<int64_t> encrypt(const std::string plaintext, const int ePublicKey, const int nPublicKey) {
+    std::vector<int64_t> ciphertext;
+
+    for (char c : plaintext) {
+        ciphertext.push_back(modPow(c, ePublicKey, nPublicKey));
     }
-    return decryptedMsg;
+    return ciphertext;
 }
 
-int main(){
-    std::string testString = "Hello, world!";
-    std::vector<uint64_t> testEncryptedMessage;
+std::string decrypt(const std::vector<int64_t> ciphertext, const int dPrivateKey, const int nPublicKey) {
+    std::string plaintext;
+
+    for (int64_t i : ciphertext) {
+        plaintext.push_back(modPow(i, dPrivateKey, nPublicKey));
+    }
+    return plaintext;
+}
+
+int main() {
+    const std::string plaintext = "Hello, world!";
+
     // TODO: Random prime numbers
-    int firstprime = 53;
-    int secondprime = 59;
-    int nPublicKey = firstprime*secondprime; // 3127
-    int ePublicKey = 3; // For nPublicKey = 3127, assume ePublicKey = 3
-    int phiNum = 3016; // assume phi(3127) = 3016
-    int k = 2; // assume k = 2
-    int dPrivateKey = ((k * phiNum + 1) / ePublicKey); // 2011 
-    encryption(testString, testEncryptedMessage, ePublicKey, nPublicKey);
-    std::cout << decryption(testEncryptedMessage, dPrivateKey, nPublicKey);
+    const int firstPrime = 53;
+    const int secondPrime = 59;
+    const int nPublicKey = firstPrime * secondPrime; // 3127
+    const int ePublicKey = 3; // For nPublicKey = 3127, assume ePublicKey = 3
+    const int phiNum = (firstPrime - 1) * (secondPrime - 1); // assume phi(3127) = 3016
+    const int k = 2; // assume k = 2
+    const int dPrivateKey = ((k * phiNum + 1) / ePublicKey); // 2011
+
+    const std::vector<int64_t> ciphertext = encrypt(plaintext, ePublicKey, nPublicKey);
+    std::cout << decrypt(ciphertext, dPrivateKey, nPublicKey);
 }
