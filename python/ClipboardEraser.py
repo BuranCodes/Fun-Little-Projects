e4c173a8233900370313f0001ab21ccd27f6fd4a
BuranCodes <95971666+BuranCodes@users.noreply.github.com>
2023-02-19 13:55:47 +0100

    Updated ClipboardEraser.py

--- python/ClipboardEraser.py
+++ python/ClipboardEraser.py
@@ -7,7 +7,7 @@ import keyboard
 
 # The method: Clear after one paste, special thanks to 404_beef on Discord
 def clear_clipboard(e):
-    line = ', '.join(str(code) for code in keyboard._pressed_events) #! Issue: You need to double copy, then paste. Copying once won't copy anything.
+    line = ', '.join(str(code) for code in keyboard._pressed_events)
     # '\r' and end='' overwrites the previous line.
     # ' '*40 prints 40 spaces at the end to ensure the previous line is cleared.
     print('\r' + line + ' ' * 40, end='')
