2218192a433746e562176eb025ea3e2321f8f945
BuranCodes <95971666+BuranCodes@users.noreply.github.com>
2023-03-09 23:04:55 +0100

    Update wackyvariable.c

--- C/wackyvariable.c
+++ C/wackyvariable.c
@@ -1,11 +1,12 @@
 // Buran
-// How to undefined behaviour easy style! y is just random register
+// How to undefined behaviour easy style! y is just garbo number
 #include <stdio.h>
 
-int main() {
-    int x = 5;
-    int y = y+x;
-    printf("%d", y);
+int main()
+{
+        int x = 5;
+        int y = y+x;
+        printf("%d", y);
 }
 
 // Undefined behaviour!
