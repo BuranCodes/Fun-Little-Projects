3b5af573456e3d1ddb1dc09e7a32d4e26c6b8de1
BuranCodes <95971666+BuranCodes@users.noreply.github.com>
2023-03-09 22:50:14 +0100

    Update commandlineargcs.c

--- C/commandlineargcs.c
+++ C/commandlineargcs.c
@@ -2,7 +2,8 @@
 // Command line arguments
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
         printf("Program name: %s\n", argv[0]);
         if (argc == 2)
                 printf("The argument supplied is %s\n", argv[1]);
